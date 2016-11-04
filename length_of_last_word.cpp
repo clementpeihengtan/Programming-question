189d550470ccb5e18d5521e143aae390604dd5f6
Clement Tan <clementtph@gmail.com>
2016-11-03 22:52:15 -0500

    Update second solution

--- length_of_last_word.cpp
+++ length_of_last_word.cpp
@@ -2,7 +2,7 @@
 //example: Given s = "Hello World"
 //return 5
 
-
+//solution 1
 int lengthoflastword(String s){
   int len = 0;
   int endindex = s.length()-1;
@@ -18,3 +18,20 @@ int lengthoflastword(String s){
 }
 
 //this algorithm takes O(n) where n is the length of the string s.
+
+
+//solution 2
+int lengthoflastword(String s){
+  int len = 0, curlen = 0, lastlen = 0;
+  for(int i = 0; i < s.length()-1 ; i++){
+     if(isalpha(s[i])){
+       curlen++;
+     }
+    else if (curlen != 0){
+       lastlen = curlen;
+       curlen = 0;
+    }
+  }
+  return curlen == 0? lastlen : curlen
+
+    //this algorithm takes O(n) as well where n is the length of the string s
